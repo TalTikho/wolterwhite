b1c4e871f2c833d84b37eaea33673502939e42da
yhtl350 <yotam.harari.2000@gmail.com>
2026-04-22 19:33:33 +0300

    WOL-60 Created logic for the main func dispatching commands from a map. Most of the code is commented out because we do not yet have intances of command objects.

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <string>
+using namespace std;
+
+int main (){
+    // map <string, ICommand*> commands;
+
+    // ICommand AddProduct = new AddProductCommand();
+    // commands["1"] = AddProduct;
+
+    // ICommand Recommend = new RecommendCommand();
+    // commands["2"] = help;
+
+    // ICommand Help = new HelpCommand();
+    // commands["3"] = help;
+    
+    // App app(commands);
+
+    // delete AddProduct;
+    // delete Recommend;
+    // delete Help;
+    return 0;
+
+}
