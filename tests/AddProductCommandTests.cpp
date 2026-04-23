7f0b4a473255aa03b7729a9954685083cb83ed20
yhtl350 <yotam.harari.2000@gmail.com>
2026-04-23 12:10:08 +0300

    WOL-43 Chnaged the test to make sure it fits the changes in ICommand and main accepting args in add's constructor.

--- tests/AddProductCommandTests.cpp
+++ tests/AddProductCommandTests.cpp
@@ -1,10 +1,19 @@
 #include <gtest/gtest.h>
-#include <AddProductCommand.h> 
+#include <sstream>
+#include "FileDataStorage.h"
+#include "AddProductCommand.h"
+
+static const std::string TEST_FILE = "data/test_storage.txt";
 
 TEST (AddProductTest, ValidSingleProduct){
-    FileDataStorage Loader;
-    AddProductCommand add (Loader);
-    //Enter a user 1 the product 101.
-    add.execute(); 
-    Assert_True(add.getLoader.find(1,101));
+    //Set user input for parsing into command and args.
+    std:: string  args = "1 101";
+    std:: istringstream argsStream(args);
+    //Load the file into the command object.
+    FileDataStorage loader(TEST_FILE);
+    AddProductCommand add(loader);
+    //Execute accordingly on user input.
+    add.execute(argsStream); 
+    //Make sure the command was succesful.
+    EXPECT_TRUE(add.getLoader().find(1,101));
 }
