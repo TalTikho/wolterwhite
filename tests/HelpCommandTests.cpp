6707bc264ab6aa22ff79d8cfeb40401c2edb1aca
yhtl350 <yotam.harari.2000@gmail.com>
2026-05-06 15:18:38 +0300

    WOL-50
    Added tests for help. Test 2 from jira is actually tested in 1's test so I took the liberty to add a test of my own that help ignores a wrong input and returns gracefully.

---
+++ tests/HelpCommandTests.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+#include <sstream>
+#include "FileDataStorage.h"
+#include "HelpCommand.h"
+
+//Tests 1 and 2 from Jira both fit this test.
+TEST (HelpTests, shouldWork){
+    //help is the right input.
+    std:: string  args = "help";
+    //set the stream var for help.
+    std:: istringstream argsStream(args);
+    //Capture cout buffer to use in the test.
+    std:: stringstream captureBuf;
+    //Keep the original buffer to restore it for the next test.
+    std:: streambuf* originalBuf = std:: cout:: rd.buf();
+    //Capture.
+    std:: cout:: rdbuf(captureBuf.rdbuf());
+    HelpCommand h;
+    h.execute(argsStream);
+    //return the buffer after execution.
+    std::cout::rdbuf(originalBuf);
+    //Make sure the command was succesful.
+    ASSERT_EQ(captureBuf.str(), " add [userid] [productid1] [productid2] …\nrecommend [userid] [productid]\nhelp\n");
+}
+
+//Added a test to ignore wrong input.
+
+TEST (HelpTests, shouldIgnore){
+    //help is the right input.
+    std:: string  args = "help me I need somebody HELP!";
+    //set the stream var for help.
+    std:: istringstream argsStream(args);
+    //Capture cout buffer to use in the test.
+    std:: stringstream captureBuf;
+    //Keep the original buffer to restore it for the next test.
+    std:: streambuf* originalBuf = std:: cout:: rd.buf();
+    //Capture.
+    std:: cout:: rdbuf(captureBuf.rdbuf());
+    HelpCommand h;
+    h.execute(argsStream);
+    //return the buffer after execution.
+    std::cout::rdbuf(originalBuf);
+    //Make sure the command was not sucessful but we can call it again later.
+    ASSERT_NE(captureBuf.str(), " add [userid] [productid1] [productid2] …\nrecommend [userid] [productid]\nhelp\n");
+    h.execute(argsStream);
+}
