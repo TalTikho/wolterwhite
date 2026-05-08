dca6492300e1e7aaa64303aed4bd8962859e251e
yhtl350 <yotam.harari.2000@gmail.com>
2026-05-08 15:08:50 +0300

    WOL-118
    Map at returns error so changed it to iterator and or find in App.cpp and RecommendCommand.cpp

--- src/App.cpp
+++ src/App.cpp
@@ -5,7 +5,6 @@
 #include <string>
 #include <vector>
 
-
 /*App's constructor, the map is not initiallized here but in register_command (this might change afterwards)
  We have a general menu accepted here as any class implementing IMenu can be this private variable.
 */
@@ -17,48 +16,55 @@ App::App(IMenu *m)
 
 void App::run() noexcept
 {
-    //The program starts with a help menu to avoid confusion.
-    std:: istringstream help;
-    this->cmds.at("help")->execute(help);
-    while (true){
-        std:: string command = menu->nextCommand();
-        //A line only for the tests to break the loop and continue to the next test.
-        if (command == "quit"){
+    // The program starts with a help menu to avoid confusion.
+    if (this->cmds.count("help"))
+    {
+        std::istringstream help;
+        this->cmds.at("help")->execute(help);
+    }
+    while (true)
+    {
+        std::string command = menu->nextCommand();
+        // A line only for the tests to break the loop and continue to the next test.
+        if (command == "quit")
+        {
             break;
         }
-        //Do not accept tabs.
-        if (command.find('\t') != std::string:: npos ){
+        // Do not accept tabs.
+        if (command.find('\t') != std::string::npos)
+        {
             continue;
         }
-        //Start a stream to validate the input sent to the command.
-        std:: istringstream ss(command);
-        std :: string cmd;
-        std:: string cache;
+        // Start a stream to validate the input sent to the command.
+        std::istringstream ss(command);
+        std ::string cmd;
+        std::string cache;
         ss >> cmd;
-        //We do not want the leftover whitespace after the valid command accepted into the command's method.
-        ss >> std:: ws;
+        // We do not want the leftover whitespace after the valid command accepted into the command's method.
+        ss >> std::ws;
         /*Try the command and if it exists and the args are valid it works. Else: back to the loop
          Even if there is an error we just continue the loop with the next query for the next command.
-         .at(cmd) will make sure that if cmd is not in our cmds map a null pointer will not be created, 
+         .at(cmd) will make sure that if cmd is not in our cmds map a null pointer will not be created,
          there is no segfault and the loop goes on with catch's continue.
          */
-        try {
-            this->cmds.at(cmd)->execute(ss);
+        if (this->cmds.count(cmd))
+        {
+            try
+            {
+                this->cmds.at(cmd)->execute(ss);
+            }
+            catch (...)
+            {
+                continue;
+            }
         }
-        catch (...){
-            continue;
-        }
-        //App never stops (except when tested).
-
-
-
+        // App never stops (except when tested).
     }
 }
 
 /*Add a new command to the app. If it exists run will tell the command to execute.
  Otherwise: the execution fails and we continue run's loop. */
-void App::registerCommand(std::string name, ICommand& com)
+void App::registerCommand(std::string name, ICommand &com)
 {
     this->cmds[name] = &com;
-
 }
