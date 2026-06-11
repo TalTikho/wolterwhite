dd542a7da528db41ed4d1016682f619196db42fd
yhtl350 <yotam.harari.2000@gmail.com>
2026-06-11 12:19:06 +0300

    WOL-276
    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 import React from 'react';
-import fetch, { ApiExample } from './components/fetch.js'
+import  { ApiExample } from './components/Fetch.js'
+import { Home } from './components/Home.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>src/App.js</code> 
-        </p>
-        <p><ApiExample /></p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //BrowserRouter acts as the master wrapper that watches the URL
+    <BrowserRouter>
+      {/*Anything placed outside <Routes> (like a NavBar) will show on EVERY page */}
+      <nav style={{ padding: '10px', background: '#333', color: 'yellow' }}>
+        <h3>Wolterwhite Delivery</h3>
+        <Link to="/">Home</Link> |{" "}
+        <Link to="/example">Watch Our beautiful restaurant list (ApiExample)</Link> |{" "}
+      </nav>
+
+      {/*The Routes block acts as the map. It looks at the URL and chooses one Route to draw */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/example" element={<ApiExample />} />
+
+        {/*<Route path="/login" element={<Login />} /> */}
+        {/* <Route path="/restaurants" element={<RestaurantList />} /> */}
+        {/*A catch-all route for bad URLs (to be a page)*/}
+        <Route path="*" element={<h2>404 - Page Not Found - Better Call Saul! (505) 503-4455 </h2>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../index.css'; 
+
+export const Home = () => {
+
+    return (
+        <div>
+            <h3>Wolterwhite</h3>
+            <div className="Home">
+            </div>
+
+        </div>
+    );
+}
