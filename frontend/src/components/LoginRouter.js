a483c3f318f7a38eaeaf1043d66eb4fcc3344a0e
yhtl350 <yotam.harari.2000@gmail.com>
2026-06-11 20:12:23 +0300

    WOL-276
    alert of protected route should only pop up if the user is not signed in.

--- frontend/src/components/LoginRouter.js
+++ frontend/src/components/LoginRouter.js
@@ -1,12 +1,12 @@
 import { Navigate } from 'react-router-dom';
 
 export const ProtectedRoute = ({ component }) => {
-    //Popup alert to user.
-    alert("ProtectedRoute hit");
     const token = localStorage.getItem("token");
 
     //No token means no connected user so he has to login.
     if (!token) {
+        //Popup alert to user alerting him to sign in.
+        alert("Protected Route hit please sign in");
         return <Navigate to="/login" />;
     }
 
