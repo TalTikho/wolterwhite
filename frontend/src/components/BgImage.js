23f190c8a372f446266b3814627047fc02efa848
TalTikho <196236000+TalTikho@users.noreply.github.com>
2026-06-15 21:46:58 +0300

    back to reguklar

--- frontend/src/components/BgImage.js
+++ frontend/src/components/BgImage.js
@@ -1,18 +1,15 @@
 export const BackG = () => {
-    return (
-        <div
-            style={{
-                position: 'fixed',
-                top: 0,
-                left: 0,
-                width: '100vw',
-                height: '100vh',
-                backgroundImage: 'url("/knock.png")',
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                backgroundRepeat: 'no-repeat',
-                zIndex: -1,
-            }}
-        />
-    );
+  return (
+    <img
+      /* Assets in the public folder are served at the root URL '/' so we make absolute linkage here */
+      src="/knock.png"
+      alt="I am the one who knocks"
+      style={{
+        width: "100%",
+        maxWidth:
+          "1200px" /* Prevents it from getting too massive on huge screens */,
+        height: "auto" /* Maintains perfect proportions */,
+      }}
+    />
+  );
 };
