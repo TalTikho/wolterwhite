3837fd9dbfe3be170882de29681656e992e41bba
yhtl350 <yotam.harari.2000@gmail.com>
2026-06-17 16:27:29 +0300

    WOL-302
    welcoming bootstrap toast notification

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -4,32 +4,54 @@ import '../style/index.css';
 import { useEffect } from 'react';
 import { BackG } from '../components/BgImage';
 import { useAuthContext } from '../context/AuthContext';
+import { Toast, ToastContainer } from 'react-bootstrap';
+import { useState } from 'react';
 
 export const Home = () => {
 
-        const { token } = useAuthContext();
-        //payload is in the middle, we need to convert it from base64.
-        const payload = JSON.parse(atob(token.split(".")[1]));
-        //get the username from the payload.
-        const username = payload.username;
-        //useEffect to inject the alert js method into the Homepage 
-        //without blocking the rendering alltogether.
-        //justLoggedIn is no longer neccesary. We want to
-        //welcome the user only once after login.
-        useEffect(() => {
-            if (localStorage.getItem("justLoggedIn")) {
-                alert("Welcome " + username);
-                localStorage.removeItem("justLoggedIn"); // clear it so it won't show again
-            }
-        }, [username]);
+    const { token } = useAuthContext();
+    //payload is in the middle, we need to convert it from base64.
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    //get the username from the payload.
+    const username = payload.username;
+    const [welcome, setWelcome] = useState(false);
+    //useEffect to inject the alert js method into the Homepage 
+    //without blocking the rendering alltogether.
+    //justLoggedIn is no longer neccesary. We want to
+    //welcome the user only once after login.
+    useEffect(() => {
+        if (localStorage.getItem("justLoggedIn")) {
+            setWelcome(true);
+            localStorage.removeItem("justLoggedIn"); // clear it so it won't show again
+        }
+        //run exactly once whenthe page opens.
+    }, []);
     return (
-
         <div>
-            <h3>Wolterwhite</h3>
-            <p>Welcome, {username}!</p>
-            <div className="Home">
-                <BackG />
+            <div>
+                <h3>Wolterwhite</h3>
+                <div className="Home">
+                    <BackG />
+                </div>
             </div>
+            <ToastContainer className="p-3"
+                position="top-center"
+                style={{ position: 'fixed', top: 0, zIndex: 9999, pointerEvents: 'none', color: "#0bd20b" }}>
+                <Toast show={welcome} onClose= {() => setWelcome(false)}
+                    delay={5000}
+                    autohide
+                    style={{
+                        pointerEvents: 'auto',
+                        backgroundColor: '#C4BE00', // Wolt's gown color
+                        color: '#1C4028' // The right green color           
+                    }}>
+                    <Toast.Header>
+                        <strong className="me-auto">Welcome to WolterWhite</strong>
+                        <small>Just now</small>
+                    </Toast.Header>
+                    <Toast.Body>Yeah, Mr. {username}! Yeah, Science!</Toast.Body>
+                </Toast>
+            </ToastContainer>
         </div>
 
 
